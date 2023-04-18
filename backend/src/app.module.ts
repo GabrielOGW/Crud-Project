fa4d5cb0a48a85544c9a9d643a8901ec4b329f3e
GabrielOGW <gabrielogassawara2001@gmail.com>
2023-04-18 11:26:58 -0300

    feat: importing modules to app.modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Nivel } from './nivel/entities/nivel.entity';
 import { Dev } from './devs/entities/dev.entity';
+import { NivelModule } from './nivel/nivel.module';
+import { DevsModule } from './devs/devs.module';
 
 @Module({
   imports: [
@@ -15,6 +17,8 @@ import { Dev } from './devs/entities/dev.entity';
       entities: [Nivel, Dev],
       synchronize: true,
     }),
+    NivelModule,
+    DevsModule,
   ],
 })
 export class AppModule {}
--- backend/src/nivel/nivel.controller.ts
+++ backend/src/nivel/nivel.controller.ts
@@ -10,33 +10,34 @@ import {
 import { NivelService } from './nivel.service';
 import { CreateNivelDto } from './dto/create-nivel.dto';
 import { UpdateNivelDto } from './dto/update-nivel.dto';
+import { Nivel } from './entities/nivel.entity';
 
 @Controller('nivel')
 export class NivelController {
   constructor(private readonly nivelService: NivelService) {}
 
   @Post()
-  create(@Body() createNivelDto: CreateNivelDto) {
+  create(@Body() createNivelDto: CreateNivelDto): Promise<Nivel> {
     return this.nivelService.create(createNivelDto);
   }
 
-  @Get("/users")
-  findAll() {
+  @Get()
+  findAll(): Promise<Nivel[]> {
     return this.nivelService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.nivelService.findOne(+id);
+  findOne(@Param('id') id: number): Promise<Nivel> {
+    return this.nivelService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNivelDto: UpdateNivelDto) {
-    return this.nivelService.update(+id, updateNivelDto);
+  update(@Param('id') id: number, @Body() updateNivelDto: UpdateNivelDto) {
+    return this.nivelService.update(id, updateNivelDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.nivelService.remove(+id);
+  remove(@Param('id') id: number): Promise<void> {
+    return this.nivelService.remove(id);
   }
 }
