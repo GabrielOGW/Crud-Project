a282d696d27691974875bfe87473f7b21cd3a2ab
GabrielOGW <gabrielogassawara2001@gmail.com>
2023-04-18 15:26:15 -0300

    feat: adding cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  const corsOptions: CorsOptions = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization,Origin,X-Requested-With,Accept',
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 86400,
+  };
+  app.enableCors(corsOptions);
+
   const config = new DocumentBuilder()
     .setTitle('Backend Crud')
     .setDescription('API para o crud')
